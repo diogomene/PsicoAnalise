bcea028e09af38279e6893f08cabdec21bc5fc1f
diogomene <diogodelimamenezes15@gmail.com>
2021-10-03 22:39:03 -0400

    Continuando construção de componentes da tela inicial da aplicação; configurando a inicialização da tela da aplicação do electron

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const {app, BrowserWindow, Tray, Menu} = require('electron')
 let mw
 
 app.on('ready', ()=>{
-    const tray = new Tray(path.join(__dirname, 'assets', "icon.png"))
+    const iconPath = path.join(__dirname, 'assets', "icon.png")
+    const tray = new Tray(iconPath)
     const ctxMenu = Menu.buildFromTemplate([{label:"PsicoAnalise", type:'radio', checked:true}])
     tray.setContextMenu(ctxMenu)
-    mw = new BrowserWindow({})
+    mw = new BrowserWindow({width:800, height:525, center:true, resizable:false, icon:iconPath, autoHideMenuBar: true})
     mw.loadURL(`file://${__dirname}/index.html`)
 })
